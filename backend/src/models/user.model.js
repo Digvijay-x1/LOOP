8bc40002a68ed7ebf42ce4919ada0f20eab64533
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-02 17:55:58 +0530

    rem the .git

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bannerImage: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+      maxlength: 160,
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello From LOOP server');
+});
+
+app.listen(ENV.PORT,  () => {
+    console.log(`Server is running on port ${ENV.PORT}`);
+    connectDB();
+});
