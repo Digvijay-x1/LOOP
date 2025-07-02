70519f974aeac9b407c17a600758e8019484712a
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-02 22:12:42 +0530

    post route and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import { connectDB } from './config/db.js';
 import cors from "cors";
 import {clerkMiddleware} from "@clerk/express"
 import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 
 const app = express();
 app.use(cors());
@@ -11,10 +12,16 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 app.use("/api/users",userRoutes)
+app.use("/api/posts", postRoutes)
 app.get('/', (req, res) => {
     res.send('Hello From LOOP server');
 });
 
+// error handling middleware
+app.use((err, req, res) => {
+    console.error("Error in post routes", err);
+    res.status(500).json({ error: "Something went wrong" });
+  });
 app.listen(ENV.PORT,  () => {
     console.log(`Server is running on port ${ENV.PORT}`);
     connectDB();
