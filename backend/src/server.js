9f481dcdd59a14ab196a5060bb828e01d135508a
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-02 23:25:21 +0530

    add vercel config

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,7 +28,24 @@ app.use((err, req, res , next) => {
     console.error("Error in server", err);
     res.status(500).json({ error: "Something went wrong" });
   });
-app.listen(ENV.PORT,  () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    connectDB();
-});
+
+
+  const startServer = async () => {
+    try {
+      await connectDB();
+  
+      // listen for local development
+      if (ENV.NODE_ENV !== "production") {
+        app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+      }
+    } catch (error) {
+      console.error("Failed to start server:", error.message);
+      process.exit(1);
+    }
+  };
+  
+  startServer();
+
+
+  // for vercel deployment
+  export default app;
