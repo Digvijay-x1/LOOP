37e41039554d7c5d8d551bb47c48fcdd00c6e0da
Digvijay-x1 <hackerearthx1@gmail.com>
2025-07-02 19:16:34 +0530

    user model with route and controllers

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getUserProfile, updateProfile, syncUser , getCurrentUser , followUser } from "../controllers/user.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+// public routes
+router.get("/profile/:username", getUserProfile)
+
+// protected routes
+router.post("/sync" , protectRoute, syncUser);
+router.put("/profile", protectRoute, updateProfile)
+router.post("/me", protectRoute, getCurrentUser)
+router.post("/follow/:userId", protectRoute, followUser)
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 import express from 'express'
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express"
+import userRoutes from "./routes/user.route.js"
 
 const app = express();
+app.use(cors());
+app.use(express.json())
+app.use(clerkMiddleware())
 
+app.use("/api/users",userRoutes)
 app.get('/', (req, res) => {
     res.send('Hello From LOOP server');
 });
